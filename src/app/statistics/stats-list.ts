65360b5caf01ccc4cd68dc9637a3f100bbc83913
Theo <theodrom@users.noreply.github.com>
2020-04-02 17:01:14 +0200

    Interface refactoring

--- src/app/statistics/stats-list.ts
+++ src/app/statistics/stats-list.ts
@@ -1,21 +1,54 @@
-export interface IStat {
-get: string;
-parameters: string[];
-errors: string[];
-results: number;
-response:[{
-    country: string;
-    cases: { 
-        new: string;
-        active: number;
-        critical: number;
-        recovered: number;
-        total: number;
-     };
-    deaths: { 
-        new: string;
-        total: number; };
-    day: string;
-    time: string;
-}] 
+
+export interface ICases {
+    new: string;
+    active: number;
+    critical: number;
+    recovered: number;
+    total: number;
 }
+
+export interface IDeaths {
+    new: string;
+    total: number;
+}
+
+export interface IResponse {
+    country: string;
+    cases: ICases;
+    deaths: IDeaths;
+    day: string;
+    time: Date;
+}
+
+export interface IStat {
+    get: string;
+    parameters: any[];
+    errors: any[];
+    results: number;
+    response: IResponse[];
+}
+
+
+
+
+// export interface IStat {
+// get: string;
+// parameters: any[];
+// errors: any[];
+// results: number;
+// response: [{
+//     country: string;
+//     cases: {
+//         new: string;
+//         active: number;
+//         critical: number;
+//         recovered: number;
+//         total: number;
+//      };
+//     deaths: {
+//         new: string;
+//         total: number; };
+//     day: string;
+//     time: string;
+// }];
+// }
