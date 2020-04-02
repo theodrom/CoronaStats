78302b98ee0fb97443a801c65b98a79915cce0ab
Theo <theodrom@users.noreply.github.com>
2020-04-02 17:00:51 +0200

    Added filter by country name (not ready yet)

--- src/app/statistics/stats-list.component.ts
+++ src/app/statistics/stats-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { IStat } from './stats-list';
-import { StatsService } from './stats.service'
+import { IStat, IResponse } from './stats-list';
+import { StatsService } from './stats.service';
 
 @Component({
     selector: 'corStats-list',
@@ -10,16 +10,37 @@ import { StatsService } from './stats.service'
 
 export class CoronaStatsListComponent implements OnInit{
 
-    corStats: IStat[];
+    corStats: IStat;
+    countryResp: IResponse[];
     error: string;
 
     constructor(private statsService: StatsService) {}
 
+    _countryFilter = '';
+    get countryStats(): string{
+        return this._countryFilter;
+    }
+    set countryStats(value: string){
+        this._countryFilter = value;
+        this.corStats.response = this.countryStats ? this.filterStatsByCountry(this.countryStats) : this.corStats.response;
+    }
+    filterStatsByCountry(countryFilter: string): IResponse[]{
+        countryFilter = countryFilter.toLocaleLowerCase();
+        return this.corStats.response.filter((responseStat: IResponse) =>
+            responseStat.country.toLocaleLowerCase().includes(countryFilter)
+        );
+    }
+
     ngOnInit(): void {
         this.statsService.getAllCountryStats().subscribe({
-            next: data => this.corStats = data,
+            next: data => {
+                this.corStats = data;
+                this.corStats.response.sort((a, b) => {
+                    return b.cases.total - a.cases.total;
+                });
+            },
             error: (err: string) => this.error = err,
-            complete: () => console.log('Coplited')
+            complete: () => console.table(this.corStats)
         });
     }
 }
