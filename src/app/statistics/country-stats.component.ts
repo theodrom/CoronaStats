3db982d97bff03664cd33993fd27811afe9c8e24
theodrom <theodrom@yahoo.gr>
2020-04-06 00:33:38 +0200

    Added country-stats compponent

---
+++ src/app/statistics/country-stats.component.ts
@@ -0,0 +1,16 @@
+import { OnInit, Component } from '@angular/core';
+
+@Component({
+    selector: 'countryStats-app',
+    templateUrl: './country-stats.component.html',
+    styleUrls: ['./country-stats.component.scss']
+})
+
+export class CountryStatsComponent implements OnInit {
+    
+    
+    ngOnInit(): void {
+        
+    }
+
+}
