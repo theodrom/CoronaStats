b58e560e84ef88356e65b3591919a1e0a050765d
theodrom <theodrom@yahoo.gr>
2020-04-12 22:32:08 +0200

    Added routing and http call

--- src/app/statistics/country-stats.component.ts
+++ src/app/statistics/country-stats.component.ts
@@ -1,16 +1,35 @@
 import { OnInit, Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { StatsService } from './stats.service';
+import { IStat} from './stats-list';
 
 @Component({
-    selector: 'countryStats-app',
     templateUrl: './country-stats.component.html',
     styleUrls: ['./country-stats.component.scss']
 })
 
 export class CountryStatsComponent implements OnInit {
     
-    
-    ngOnInit(): void {
-        
+    constructor(private route: ActivatedRoute,
+                private router: Router,
+                private service: StatsService
+                ) {}
+
+    countryData?: IStat;
+    error: string;
+
+    onBack(): void{
+        this.router.navigate(['/statistics'])
     }
 
+    ngOnInit(): void {
+       const countryName = this.route.snapshot.paramMap.get('country').toLowerCase();
+       console.log(countryName);
+
+        this.service.getStatsByCountryName(countryName).subscribe({
+            next: countryData => this.countryData = countryData,
+            error: err => this.error = err,
+            complete: () => console.log(this.countryData)
+        });
+    }
 }
